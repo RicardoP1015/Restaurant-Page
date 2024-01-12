bdf2e43f125db28991e1d40ceb4d9ef35441af38
RicardoP <picoricardo3d@outlook.com>
2024-01-12 03:35:51 -0500

    Showcase section about the animatronics was made and create img function
    made a function that help create all of the animatronics images for the
    showcase section the function creates a img element takes
    then image from the assets and the div of where the image will
    go by its class and append the img to the wrapper and then
    return the new image

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
-import './styles/style.css'
+import './styles/style.css';
 import headerLogo from './assets/pizzaria.png';
-import williamAftonImg from './assets/WilliamAfton.png'
-import fanfIcon from './assets/FNAF-bg.png'
+import williamAftonImg from './assets/WilliamAfton.png';
+import fnafIcon from './assets/FNAF-bg.png';
+import fnafFreddy from './assets/freddyFNAF.png';
+import fnafBonnie from './assets/bonnieFNAF.png';
+import fnafChica from './assets/chicaFNAF.png';
+import fnafFoxy from './assets/foxyFNAF.png';
 
-const logoContainer = document.querySelector('.logo')
+const headerContainer = document.querySelector('.logo')
 const ownerImg = document.querySelector('.owner-img')
 const aboutUsImg = document.querySelector('.about-us-img')
+const freddyImg = document.querySelector('.freddy')
+const bonnieImg = document.querySelector('.bonnie')
+const chicaImg = document.querySelector('.chica')
+const foxyImg = document.querySelector('.foxy')
 
-const pizzariaLogo = new Image();
-const ownerWilliamA = new Image();
-const fanfImg = new Image(); 
 
 
 
-pizzariaLogo.src = headerLogo;
-ownerWilliamA.src = williamAftonImg;
-fanfImg.src = fanfIcon;
+const CreateImage = (img,wrapper) => {
+    const imgNew = new Image();
+    imgNew.src = img;
+    return wrapper.appendChild(imgNew);
+}
 
-logoContainer.appendChild(pizzariaLogo);
-ownerImg.appendChild(ownerWilliamA);
-aboutUsImg.appendChild(fanfImg)
+const mainLogo = CreateImage(headerLogo, headerContainer);
+const ownerImage = CreateImage(williamAftonImg, ownerImg);
+const fnafBanner = CreateImage(fnafIcon, aboutUsImg);
+const freddyImage = CreateImage(fnafFreddy, freddyImg);
+const bonnieImage = CreateImage(fnafBonnie, bonnieImg);
+const chicaImage = CreateImage(fnafChica, chicaImg);
+const foxyImage = CreateImage(fnafFoxy, foxyImg);
+
+
+
 
 
